1d2b60b3a22ba3a10f2da97638eda3b26ecb0b92
Kapranov <lugatex@yahoo.com>
2013-01-22 16:43:40 +0200

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def fix_form
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FixCustomer::Application.config.secret_token = '5e941c4413d64df267df8c9b2ad3befab7487eb02099878d7a080aedead8ce0815d758d4489ed1b1c57b1b9d8210f090873cdecd9a2aae49aba72340f0cf70fd'
